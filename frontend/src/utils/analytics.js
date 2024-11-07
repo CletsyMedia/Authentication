ffcd7a7e5ee886087f4c7284bb2d97180673c2f1
CletsyMedia <Cletussam12@yahoo.com>
2024-11-07 07:56:26 +0100

    Updated GA Tracking ID

--- frontend/src/utils/analytics.js
+++ frontend/src/utils/analytics.js
@@ -1,7 +1,7 @@
 import ReactGA from 'react-ga';
 
 export const initializeGA = () => {
-  const trackingId = process.env.REACT_APP_GA_TRACKING_ID;
+  const trackingId = process.env.GA_TRACKING_ID;
   if (trackingId) {
     ReactGA.initialize(trackingId);
   } else {
