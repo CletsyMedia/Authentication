d5656cb8d7935026f587b65ab71d6684aaabd1ec
CletsyMedia <Cletussam12@yahoo.com>
2024-11-07 14:00:05 +0100

    Update GA

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-TH95C9W57X");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
